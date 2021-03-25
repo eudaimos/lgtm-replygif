03219d4d9878c1f5c7986d8b7bcccfd3f7be7af2
Jeff Hoffer <jeff.hoffer@rollbar.com>
2021-03-24 21:00:07 -0700

    updated rollbar config

--- src/utils/rollbar/config.js
+++ src/utils/rollbar/config.js
@@ -4,6 +4,8 @@ const { ROLLBAR_ACCESS_TOKEN } = process.env;
 
 const config = {
   accessToken: ROLLBAR_ACCESS_TOKEN,
+  code_version: '3377043821c5c8575b21c3b409a4c14674ed1d0b',
+  environment: 'production',
   captureUncaught: true,
   captureUnhandledRejections: true,
   includeItemsInTelemetry: true,
